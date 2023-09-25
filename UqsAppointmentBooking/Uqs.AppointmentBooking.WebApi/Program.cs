b4ca730667e5b33dfe6a9487963de3fb4c5efc29
carlos-regis <zeca2078@gmail.com>
2023-09-25 20:51:38 +0000

    Wire up the WebApi with EF

--- UqsAppointmentBooking/Uqs.AppointmentBooking.WebApi/Program.cs
+++ UqsAppointmentBooking/Uqs.AppointmentBooking.WebApi/Program.cs
@@ -1,12 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Uqs.AppointmentBooking.Domain.Database;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var services = builder.Services;
 
-//services.AddDbContext<ApplicationContext>(options =>
-//    options.UseSqlServer(
-//        builder.Configuration.GetConnectionString("AppointmentBooking")
-//    ));
+services.AddDbContext<ApplicationContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("AppointmentBooking")
+    ));
 
 services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
